d39d6034158f23615cfd88bb4d1bb6524d79e8a8
Shivakumar R <shivakumar.r@nocodez.com>
2023-02-07 21:45:55 +0530

    Add lazyCoulmn and basic animations

--- app/src/main/java/com/example/jetpackcomposebasics/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/ui/theme/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.jetpackcomposebasics.ui.theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,7 +50,7 @@ fun MyApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun MyApp2(modifier: Modifier = Modifier) {
-    var showOnBoarding by remember { mutableStateOf(true) }
+    var showOnBoarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier = modifier) {
         if (showOnBoarding) {
@@ -53,18 +59,33 @@ fun MyApp2(modifier: Modifier = Modifier) {
     }
 }
 
+//@Composable
+//fun MyAppColumn(
+//    modifier: Modifier = Modifier,
+//    names: List<String> = listOf("Compose", "World")
+//) {
+//    Column(
+//        modifier = modifier
+//            .padding(vertical = 4.dp, horizontal = 8.dp)
+//            .fillMaxSize()
+//    ) {
+//        for (item in names) {
+//            Greeting2(name = item)
+//        }
+//    }
+//}
+
 @Composable
 fun MyAppColumn(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Compose", "World")
+    names: List<String> = List(500) { "$it" }
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier
-            .padding(vertical = 4.dp, horizontal = 8.dp)
-            .fillMaxSize()
+            .padding(vertical = 4.dp)
     ) {
-        for (item in names) {
-            Greeting2(name = item)
+        items(items = names) { name ->
+            Greeting2(name = name)
         }
     }
 }
@@ -73,21 +94,32 @@ fun MyAppColumn(
 fun Greeting2(name: String) {
     //use mutableStateof in compose
     //use remember so it doesn't reset on every recomposition
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 40.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 50.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Row(modifier = Modifier.padding(20.dp)) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp)
+        ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello ")
                 Text(text = "$name!")
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
